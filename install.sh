e8aa4e057d5063b680ac3b6001acda6e9b11d38b
Jeremy Lefurge <jerelefurge@gmail.com>
2017-01-18 21:36:34 -0500

    Added xinit to package downloads

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ repos () {
     git clone https://github.com/Airblader/i3.git ~/.i3gapsrepo/i3-gaps/
     cd ~/.i3gapsrepo/i3-gaps/
 	# Dependencies
-	sudo apt -y install libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev dh-autoreconf
+	sudo apt -y install libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev dh-autoreconf xinit
 	# compile & install
 	autoreconf --force --install
 	rm -rf build/
